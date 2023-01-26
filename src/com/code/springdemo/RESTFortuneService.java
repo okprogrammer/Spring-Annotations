4a24da438d6fbec523d1a1d5074c0606d8d99506
Om Kumar <om.kumar.-nd@disney.com>
2023-01-27 00:37:27 +0530

    using qualifier annotation for multiple bean impl

---
+++ src/com/code/springdemo/RESTFortuneService.java
@@ -0,0 +1,14 @@
+package com.code.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RESTFortuneService implements FortuneService {
+
+	@Override
+	public String getFortune() {
+		
+		return null;
+	}
+
+}
